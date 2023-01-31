95ceb0166793ba444fa15c63fcc857a3060be5e1
Joe Küng <joekueng05@gmail.com>
2023-01-31 15:18:29 +0100

    added OnInit

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit{
+
+  ngOnInit(): void {
+  }
 
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
-
+export class ListComponent implements OnInit {
+  ngOnInit(): void {
+  }
 }
