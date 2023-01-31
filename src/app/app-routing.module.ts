f2dab91a1dca81e297d0b00ca757ed0db06889d7
Joe Küng <joekueng05@gmail.com>
2023-01-31 14:30:30 +0100

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {DetailComponent} from "./detail/detail.component";
 
 
 @NgModule({
   imports: [RouterModule.forRoot([
     {path: 'home', component: HomeComponent},
+    {path: 'detail', component: DetailComponent},
     {path: '**', redirectTo: 'home'}
   ])],
   exports: [RouterModule],
