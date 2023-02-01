36bfb90a29510ca5b8821d9cd0c90969edb43575
Joe Küng <joekueng05@gmail.com>
2023-02-01 13:05:17 +0100

    translate service created

---
+++ src/app/service/translate.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Translate } from '@google-cloud/translate/build/src/v2';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TranslateService {
+
+  private translate: Translate;
+
+  constructor() {
+    this.translate = new Translate({projectId: 'Modulo-152'});
+  }
+
+  async translateText(text: string, target: string): Promise<string> {
+    const [translation] = await this.translate.translate(text, target);
+    return translation;
+  }
+
+}
